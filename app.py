5f443f7337d0446401c6e610b6320c6e6cde7495
Willian Picão <willianpicao6@gmail.com>
2023-01-23 12:13:11 -0300

    feat: hypotenuse calculator

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template, request
-
+from math import hypot
 
 app= Flask(__name__)
 
+#starting route
 @app.route('/')
 def index():
     return render_template('base.html')
 
+#this route loads the calculator layout
 @app.route('/calcaguras')
 def calcaguras():
     return render_template('calcaguras.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():  #function that calculates the hypotenuse
+    sideA= int(request.form['sideA']) #takes the value filled in the form referring to the side A
+    sideB= int(request.form['sideB']) #takes the value filled in the form referring to the side B
+
+    hypotenuseValueResult= round(hypot(sideA,sideB),2)#hypot calculates the hypotenuse and round rounds the decimal places
+
+    return f"<h1 style='background-image: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));color:white;width: 100vw;height: 100vh;display: flex;flex-direction: row;justify-content: center;align-items: center;'>Hypotenuse Value= "+ str(hypotenuseValueResult)+ "</h1>"
